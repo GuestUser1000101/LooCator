73644b5b42d84e79b1a3ca3cdaf422378d1a600c
MichaelFine2022 <mf9947967@gmail.com>
2025-02-16 15:54:44 -0800

    final filter

--- Map.js
+++ Map.js
@@ -10,6 +10,21 @@ slider.oninput = function() {
     pText.textContent = this.value;
 }
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 var map = L.map('map').setView([-25.2744, 133.7751], 5);
 
 const SampleRestrooms = [
